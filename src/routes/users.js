42259ee8a84f4eba018429881814ab5457c5e397
Xavier Paredes <jxavierpp1@gmail.com>
2020-06-09 23:28:53 -0700

    adding login/logout routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
-const router = Router();
 const { getUser, getUsers, createUser, updateUser, deleteUser } = require('../controllers/users.controller');
+const authController = require('../controllers/authController');
+const router = Router();
+
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 router.route('/')
     .get(getUsers)
